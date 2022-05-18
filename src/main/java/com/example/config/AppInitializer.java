0c5ccbf1f68dccd65e5ee4acb61f0c37fca7ec12
dev-rashed-alam <dev.rashedalam@gmail.com>
2022-05-19 00:57:14 +0600

    add xml based hibernate configuration

--- src/main/java/com/example/config/AppInitializer.java
+++ src/main/java/com/example/config/AppInitializer.java
@@ -6,21 +6,29 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+
 
 public class AppInitializer implements WebApplicationInitializer {
     @Override
-    public void onStartup(ServletContext servletContext) {
-        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        rootContext.register(RootConfig.class);
-        rootContext.refresh();
-        servletContext.addListener(new ContextLoaderListener(rootContext));
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        // root config
+        AnnotationConfigWebApplicationContext objRootConfig = new AnnotationConfigWebApplicationContext();
+        objRootConfig.register(RootConfig.class, DbConfig.class);
+        objRootConfig.refresh();
+        servletContext.addListener(new ContextLoaderListener(objRootConfig));
 
-        AnnotationConfigWebApplicationContext servletConfig = new AnnotationConfigWebApplicationContext();
-        servletConfig.register(ServletConfig.class);
-        ServletRegistration.Dynamic registration = servletContext.addServlet("servlet", new DispatcherServlet(servletConfig));
+        // servlet config
+        AnnotationConfigWebApplicationContext objRegister = new AnnotationConfigWebApplicationContext();
+        objRegister.register(ServletConfig.class);
+        ServletRegistration.Dynamic objRegistration =
+                servletContext.addServlet("servlet",new DispatcherServlet(objRegister));
 
-        registration.setLoadOnStartup(1);
-        registration.addMapping("/");
+        // loadOnStartUp
+        objRegistration.setLoadOnStartup(1);
+
+        // mapping
+        objRegistration.addMapping("/");
     }
 }
